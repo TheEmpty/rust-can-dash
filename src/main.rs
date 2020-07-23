466474c3b33fc369129484287123154ea8c718ad
Myles Empty Best <elabid@amazon.com>
2020-07-23 08:55:18 -0700

    Refactor main.rs into smaller methods

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,21 @@ use std::thread;
 use std::time::Duration;
 use rand::Rng;
 
+// TODO: evaluate if this should be user_data or if update_loop
+// can just get and pass the data without this context map.
+// Main consideration is stale data and understanding if we will do
+// "partial" updates or not.
+type CanData = HashMap<&'static str, String>;
+
 fn main() {
-    let mut can_data: HashMap<&str, String> = HashMap::new();
     let html: String = std::fs::read_to_string("dash.html").unwrap().parse().unwrap();
+    let mut view = build_web_view(html);
+    update_loop(&mut view);
+    view.run();
+}
+
+fn build_web_view(html: String) -> WebView<'static, CanData> {
+    let mut can_data: CanData = HashMap::new();
     let mut view = web_view::builder()
         .title("Rust Can Dash")
         .content(Content::Html(html))
@@ -28,7 +40,10 @@ fn main() {
         .build()
         .unwrap();
     view.set_fullscreen(true);
+    return view;
+}
 
+fn update_loop(view: &mut WebView<CanData>) {
     let handle = view.handle();
     thread::spawn(move || loop {
         {
@@ -36,15 +51,9 @@ fn main() {
                 let rand_rpm: u32 = rand::thread_rng().gen_range(0, 50) + 850; // PoC
                 let rpm_string = format!("{}", rand_rpm);
                 (*view.user_data_mut()).insert("rpm", rpm_string);
-                render(&mut view)
+                view.eval(&format!("update({:?})", view.user_data()))
             });
         }
         thread::sleep(Duration::from_millis(200)); // PoC
     });
-    view.run();
-}
-
-fn render(view: &mut WebView<HashMap<&str, String>>) -> WVResult {
-    let user_data = view.user_data();
-    view.eval(&format!("update({:?})", user_data))
 }
