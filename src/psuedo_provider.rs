34eb38ff170beaa3f525a5fc0084ffe29075a220
Myles Empty Best <elabid@amazon.com>
2020-07-26 16:25:46 -0700

    More psuedo data

--- src/psuedo_provider.rs
+++ src/psuedo_provider.rs
@@ -22,7 +22,11 @@ impl DashDataProvider for PsuedoProvider {
         can_data.insert("vss1", vss1_string);
         can_data.insert("gear", gear_string);
         can_data.insert("sensors1", 35.to_string()); // Generic Sensor in MS
+        can_data.insert("sensors2", 60.to_string());
+        can_data.insert("map", 2.to_string());
+        can_data.insert("clt", 175.to_string());
         can_data.insert("launch", true.to_string());
+        can_data.insert("pit", false.to_string());
 
         // emulate some read time
         thread::sleep(Duration::from_millis(200));
